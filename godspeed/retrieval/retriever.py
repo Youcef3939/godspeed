8b48938d2b37df8d85531b2068e19b205482edc5
chalbi youssef <youcefchalbi39@gmail.com>
2026-05-28 22:27:51 +0100

    clean up the retriever module

--- godspeed/retrieval/retriever.py
+++ godspeed/retrieval/retriever.py
@@ -1,10 +1,7 @@
 from __future__ import annotations
-
 from typing import Sequence
-
 from godspeed.domain.models import SearchHit, SearchResponse
 from godspeed.interfaces.contracts import Embedder, VectorStore
-
 
 class Retriever:
     def __init__(self, embedder: Embedder, store: VectorStore) -> None:
@@ -15,4 +12,3 @@ class Retriever:
         query_vector = self.embedder.embed([query])[0]
         hits: Sequence[SearchHit] = self.store.search(query_vector, top_k=top_k)
         return SearchResponse(query=query, hits=list(hits))
-
